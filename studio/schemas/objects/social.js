dec3e21d26f2ba531cdc6079bbfd02eed31337c6
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-08-12 09:18:25 -0600

    Add new schema for social

---
+++ studio/schemas/objects/social.js
@@ -0,0 +1,37 @@
+export default {
+  name: 'socail',
+  type: 'object',
+  title: 'Social Media Links',
+  fields: [
+    {
+      name: 'twitter',
+      type: 'url',
+      title: 'Twitter',
+      description: 'Here you would put the URL for the twitter profile'
+    },
+    {
+      name: 'instagram',
+      type: 'url',
+      title: 'Instagram',
+      description: 'Here you would put the URL for the instagram profile'
+    },
+    {
+      name: 'github',
+      type: 'url',
+      title: 'Github',
+      description: 'Here you would put the URL for the github profile'
+    },
+    {
+      name: 'linkedIn',
+      type: 'url',
+      title: 'LinkedIn',
+      description: 'Here you would put the URL for the linkedIn profile'
+    },
+    {
+      name: 'website',
+      type: 'url',
+      title: 'Website',
+      description: 'Here you would put the URL for the website profile'
+    }
+  ]
+}
